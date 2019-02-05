45c1d88ba59211b399b61d190ea4abc80154f4e9
Benedict Ng-Wai <benedict.ngwai@gmail.com>
2019-02-05 21:45:17 +0000

    background color and images, along with text setup initially

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import mongodbLogo from './mongodb.png';
+import nodejsLogo from './node-js.png';
+import expressLogo from './express.png';
+import reactLogo from './react.png';
+import htmlLogo from './html.png';
+import herokuLogo from './heroku.png';
+import css3Logo from './css3.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App'>
+        <img src={htmlLogo} />
+        <img src={reactLogo} />
+        <img src={herokuLogo} />
+        <img src={nodejsLogo} />
+        <img src={mongodbLogo} />
+        <img src={expressLogo} />
+        <img src={css3Logo} />
+          <div className='name'>
+            <h1>Benedict Ng-Wai</h1>
+              <div className='title'>
+                <p>Web Developer</p>
+                  <div className='headers'>
+                    <p>about projects contact</p>
+                  </div>
+              </div>
+          </div>
       </div>
     );
   }
